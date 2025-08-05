fd05f6424e3dc64920e17ca74d107790f857e61d
muhammad_akafe <ibusiness.21s@gmail.com>
2025-08-05 03:23:07 +0300

    Refactor user route to improve Prisma client import and streamline response structure; ensure consistent formatting and error handling.

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "../../lib/prisma";
+import { prisma } from "@/app/lib/prisma";
 
 export async function GET(request: Request) {
     try {
@@ -16,13 +16,13 @@ export async function POST(request: Request) {
     try {
         const body = await request.json();
         const { email, password } = body;
-       await prisma.user.deleteMany();
+        await prisma.user.deleteMany();
         const newUser = await prisma.user.create({
             data: {
                 username: "muhammad",
                 age: 20,
                 role: "admin",
-                email:email,
+                email,
                 password,
                 posts: {
                     create: {
@@ -37,20 +37,22 @@ export async function POST(request: Request) {
                 },
             }
         });
-        const all_users=await prisma.user.findMany();
-        const all_posts=await prisma.post.findMany({
-            where:{
-                authorId:newUser.id
+
+        const all_users = await prisma.user.findMany();
+        const all_posts = await prisma.post.findMany({
+            where: {
+                authorId: newUser.id
             }
         });
-        const all_comments=await prisma.comment.findMany({
-            where:{
-                authorId:newUser.id
+        const all_comments = await prisma.comment.findMany({
+            where: {
+                authorId: newUser.id
             }
         });
-        return NextResponse.json({ all_users: all_users, all_posts: all_posts }, { status: 201 });
+
+        return NextResponse.json({ all_users, all_posts, all_comments }, { status: 201 });
     } catch (error) {
         console.error("Database error:", error);
         return NextResponse.json({ error: "Database error occurred, " + error }, { status: 500 });
     }
-} 
+}
